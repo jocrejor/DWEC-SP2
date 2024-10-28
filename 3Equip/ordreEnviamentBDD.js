5b842982ddb8e04a046d7a1a0fdaf0a7ad1671a2
MRoig99 <155968362+MRoig99@users.noreply.github.com>
2024-10-28 11:14:58 +0100

    Base de dades Miguel Ordre enviament

---
+++ 3Equip/ordreEnviamentBDD.js
@@ -0,0 +1,42 @@
+[
+    {
+        "client_id": "Grefusa",
+        "ordershipping_status": "Pendent",
+        "carrier_id": "1001",
+        "shipping_date": 28,
+        "product": "Pipes bacon",
+        "quantity": "66"
+    },
+    {
+        "client_id": "King regal",
+        "ordershipping_status": "Pendent",
+        "carrier_id": "1002",
+        "shipping_date": 28,
+        "product": "Ametles",
+        "quantity": "87"
+    },
+    {
+        "client_id": "Grefusa",
+        "ordershipping_status": "Pendent",
+        "carrier_id": "1003",
+        "shipping_date": 28,
+        "product": "Pipes bacon",
+        "quantity": "98"
+    },
+    {
+        "client_id": "Grefusa",
+        "ordershipping_status": "Pendent",
+        "carrier_id": "1000",
+        "shipping_date": 28,
+        "product": "Pipes Tijuana",
+        "quantity": "43"
+    },
+    {
+        "client_id": "Grefusa",
+        "ordershipping_status": "Pendent",
+        "carrier_id": "1000",
+        "shipping_date": 28,
+        "product": "Pipes Tijuana",
+        "quantity": "21"
+    }
+]
