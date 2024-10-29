d03cc771bdd0517251f97eaaeb5f796e2ccc8cf2
jocrejor <jocrejor@gmail.com>
2024-10-29 17:05:07 +0100

    SISS

--- ExempleDus/llistatClients.js
+++ ExempleDus/llistatClients.js
@@ -24,16 +24,33 @@ function obtindreClients() {
 }
 function afegirLinia(ele){
     let files = document.getElementById("files");
-    let trEle= document.createElement("tr");
-    let tdEle= document.createElement("td");
-    let btnEle= document.createElement("button")
-    let btnTxt=
+    let trEli= document.createElement("tr");
+    let tdEli= document.createElement("td");
+    let btnEli= document.createElement("button");
+    let btnTxtEli= document.createTextNode("Esborrar");
+    btnEli.className="btn btn-primary btn-lg";
+    btnEli.addEventListener("click", () => esborrar(ele.id))
+    btnEli.appendChild(btnTxtEli);
+    tdEli.appendChild(btnEli);
+    trEli.appendChild(tdEli);
+
+
+    let tdMod= document.createElement("td");
+    let btnMod= document.createElement("button");
+    let btnTxtMod= document.createTextNode("Modificar");
+    btnMod.className="btn btn-primary btn-lg";
+    btnMod.addEventListener("click", () => modificar(ele.id))
+    btnMod.appendChild(btnMod);
+    tdMod.appendChild(btnMod);
+    
+
+    files.appendChild(trEli);
 
 }
 
 function esborrar (id){
     // fer les comprobacions si l'autor es pot esborrars. 
-
+        console.log(id)
     // esborrar del localstorage
 
 
