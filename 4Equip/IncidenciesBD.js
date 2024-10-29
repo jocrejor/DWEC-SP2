caa149bc1e612a2e39dbbb7974c7d028030cac0b
carmol02 <121013225+carmol02@users.noreply.github.com>
2024-10-29 09:29:44 +0100

    Update IncidenciesBD.js

--- 4Equip/IncidenciesBD.js
+++ 4Equip/IncidenciesBD.js
@@ -1,34 +1,32 @@
-const incidencia = {
-  incident: [
+let incident = [
     {
-      incident_id: "0",
-      operator_id: "3",
-      description: "Este producte esta trencat",
-      created_at: "CURRENT_TIMESTAMP"
+      "incident_id": "0",
+      "operator_id": "3",
+      "description": "Este producte esta trencat",
+      "created_at": "CURRENT_TIMESTAMP"
     },
     {
-      incident_id: "1",
-      operator_id: "6",
-      description: "El producte esta desaparegut",
-      created_at: "CURRENT_TIMESTAMP"
+      "incident_id": "1",
+      "operator_id": "6",
+      "description": "El producte esta desaparegut",
+      "created_at": "CURRENT_TIMESTAMP"
     },
     {
-      incident_id: "2",
-      operator_id: "7",
-      description: "El prodcute té el soroll distorsionat",
-      created_at: "CURRENT_TIMESTAMP"
+      "incident_id": "2",
+      "operator_id": "7",
+      "description": "El prodcute té el soroll distorsionat",
+      "created_at": "CURRENT_TIMESTAMP"
     },
     {
-      incident_id: "4",
-      operator_id: "8",
-      description: "La pantalla de prodcute no funciona",
-      created_at: "CURRENT_TIMESTAMP"
+      "incident_id": "4",
+      "operator_id": "8",
+      "description": "La pantalla de prodcute no funciona",
+      "created_at": "CURRENT_TIMESTAMP"
     },
     {
-      incident_id: "5",
-      operator_id: "9",
-      description: "La batería del producte es descarga ràpidament",
-      created_at: "CURRENT_TIMESTAMP"
+      "incident_id": "5",
+      "operator_id": "9",
+      "description": "La batería del producte es descarga ràpidament",
+      "created_at": "CURRENT_TIMESTAMP"
     }
-  ]
-};
+  ];
